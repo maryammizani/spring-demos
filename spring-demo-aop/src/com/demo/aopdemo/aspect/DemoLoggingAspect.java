cc8a927ce97e78716f4c1d5dfebe4db2a98e4b34
mmizani <maryam.mizani@gmail.com>
2021-12-23 21:15:30 -0500

    AOP JoinPoints: Display Method Signature

--- spring-demo-aop/src/com/demo/aopdemo/aspect/DemoLoggingAspect.java
+++ spring-demo-aop/src/com/demo/aopdemo/aspect/DemoLoggingAspect.java
@@ -1,8 +1,9 @@
 package com.demo.aopdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -15,8 +16,14 @@ public class DemoLoggingAspect {
 //	@Before("execution(* add*(..))")
 //	@Before("execution(* com.demo.aopdemo.dao.*.*(..))")
 	@Before("com.demo.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
-	public void beforeAddAccountAdvice() { 		
+	public void beforeAddAccountAdvice(JoinPoint iJoinPoint) { 		
 		System.out.println("\n======>>> Executing @Before advice on addAccount()");
+		
+		// Display the method signature
+		MethodSignature lMethodSignature = (MethodSignature)iJoinPoint.getSignature();
+		System.out.println("Method: " + lMethodSignature);
+		
+		// Display the method arguments
 	}
 	
 }
