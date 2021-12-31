9a6d68c67a27f77643665283bcdf0724556c7d66
mmizani <maryam.mizani@gmail.com>
2021-12-30 20:02:03 -0500

    Add Security Initializer and Config

---
+++ spring-security-demo-starter/src/main/java/com/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// Add our users for in-memory authentication
+		UserBuilder lUserBuilder = User.withDefaultPasswordEncoder();
+		auth.inMemoryAuthentication()
+		.withUser(lUserBuilder.username("john").password("test123").roles("EMPLOYEE"))
+		.withUser(lUserBuilder.username("mary").password("test123").roles("MANAGER"))
+		.withUser(lUserBuilder.username("susan").password("test123").roles("ADMIN"));
+	}
+
+}
